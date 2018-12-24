f30c3e5a538f7e174b87cf3107f6d29c8eb980b0
Artem Borisov <dedsa2002@gmail.com>
2018-12-24 15:25:51 +0400

    sdm845-common: Add QCOM's WFD implementation
    * Sadly, out-of-the-box Miracast support was killed in P, so
      we need this to keep Wi-Fi display feature working.
    
    Change-Id: Ieff24313d55a60ca1898430113f0a8b00acfc6a0

--- sdm845.mk
+++ sdm845.mk
@@ -49,7 +49,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 
 # Display
 PRODUCT_PACKAGES += \
-    libvulkan
+    libvulkan \
+    vendor.display.config@1.0
 
 # HIDL
 PRODUCT_PACKAGES += \
@@ -114,3 +115,10 @@ PRODUCT_PACKAGES += \
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk-sp
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
