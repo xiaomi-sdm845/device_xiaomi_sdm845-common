AospExtended-Devices/android_device_xiaomi_sdm845-common-1

sdm845-common: doze: Check if AOD is enabled after boot
* The service should not start if AOD has been turned on by the user.

Change-Id: I79307adbcda57e663287714ef15830da4f285131

parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- *               2017-2018 The LineageOS Project
+ *               2017-2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,10 +31,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DozeUtils.isDozeEnabled(context) && DozeUtils.sensorsEnabled(context)) {
-            if (DEBUG) Log.d(TAG, "Starting Doze service");
-            DozeUtils.startService(context);
-        }
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        DozeUtils.checkDozeService(context);
     }
-
 }

xiaomi-sdm845/device_xiaomi_sdm845-common

Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

doze/src/org/lineageos/settings/doze/BootCompletedReceiver.java
@@ -1,6 +1,10 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
+<<<<<<< HEAD:doze/src/org/lineageos/settings/doze/BootCompletedReceiver.java
  *               2017 The LineageOS Project
+=======
+ *               2017-2019 The LineageOS Project
+>>>>>>> 0f2de0d... sdm845-common: doze: Check if AOD is enabled after boot:parts/src/org/lineageos/settings/BootCompletedReceiver.java
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,10 +33,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (Utils.isDozeEnabled(context) && Utils.sensorsEnabled(context)) {
-            if (DEBUG) Log.d(TAG, "Starting service");
-            Utils.startService(context);
-        }
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        Utils.checkDozeService(context);
     }
-
 }