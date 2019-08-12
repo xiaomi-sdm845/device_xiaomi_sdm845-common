35f6c2820acacd4b82827e5b2a4df51bce742382
dinosnore1 <dino.yash0213@gmail.com>
2019-08-12 15:57:40 +0000

    Revert "Use Dirac Sound Enhancer"
    This reverts commit 6fd14cec65a1bdd491dcc3332fc5f6146ca73cf2.

--- sdm845.mk
+++ sdm845.mk
@@ -287,14 +287,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# ThermalController app
-PRODUCT_PACKAGES += \
-    ThermalController
-
-# Dirac
-PRODUCT_PACKAGES += \
-    Dirac
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
@@ -334,3 +326,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+    
+# ThermalController app
+PRODUCT_PACKAGES += \
+    ThermalController
