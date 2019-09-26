a63deff35690b6048a57d5d88c400fde2f346464
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-26 12:51:58 +0000

    beryllium: Unset custom clang version
    Change-Id: I5de694ef02aae4792d260f26e7ea3102df419627

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,7 +42,6 @@ TARGET_KERNEL_ARCH := arm64
 ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CLANG_COMPILE := true
   TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
-  TARGET_KERNEL_CLANG_VERSION := 9.0.5
 endif
 
 # Use Snapdragon LLVM, if available
