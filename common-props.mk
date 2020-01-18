de397dde88f64282d4acece057829c596abc4fa5
dinosnore1 <dino.yash0213@gmail.com>
2020-01-18 12:30:39 +0530

    sdm845-common: Force-enable vowifi
    Vowifi isnt working on carriers even with
    updated carrierconfig overlays so here we go
    
    Signed-off-by: dinosnore1 <dino.yash0213@gmail.com>

--- common-props.mk
+++ common-props.mk
@@ -62,6 +62,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Data modules
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan=1 \
+    persist.data.iwlan.ipsec.ap=1 \
     persist.data.df.dev_name=rmnet_usb0 \
     persist.vendor.data.profile_update=true \
     persist.vendor.data.mode=concurrent \
@@ -147,11 +149,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # RCS and IMS
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
-    persist.rcs.supported=0 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.rcs.supported=1 \
     persist.vendor.ims.disableUserAgent=0 \
-    persist.radio.calls.on.ims=1
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1 \
+    persist.radio.volte.dan_support=true \
+    persist.sys.cust.lte_config=true
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
